dc4fc16878e1196e35d11f92763d0a08acfa6511
Hadi <mhadimollataheri@gmail.com>
2024-04-20 12:00:35 +0330

    Create state for inputs and make Form.js controlled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 
 function App() {
+  // Create state for every three inputs so we can store and control user input
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  // Other version: using one state for all three input fields:
+  /*
+    const [inputValues, setInputValue] = useState({
+      firstName: '',
+      lastName: '',
+      email: ''
+    });
+  */
+
   return (
-    <div id="container" className="flex items-center justify-center mt-20 p-5">
-      <Form />
+    <div
+      id="container"
+      className="flex flex-col items-center justify-center mt-20 p-5"
+    >
+      <Form
+        firstName={firstName}
+        setFirstName={setFirstName}
+        lastName={lastName}
+        setLastName={setLastName}
+        email={email}
+        setEmail={setEmail}
+      />
+      {/* Tests */}
+      <p>{`I'm first name: ${firstName}`}</p>
+      <p>{`I'm last name: ${lastName}`}</p>
+      <p>{`I'm email: ${email}`}</p>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,50 @@
 import React from "react";
 
-function Form() {
+function Form(props) {
+  // Create event handlers that update the states using setter props
+  const handleFirstNameChange = ({ target }) => {
+    props.setFirstName(target.value);
+  };
+  const handleLastNameChange = ({ target }) => {
+    props.setLastName(target.value);
+  };
+  const handleEmailChange = ({ target }) => {
+    props.setEmail(target.value);
+  };
+
+  // Other version: using one state for all three input fields:
+  /*
+
+  */
+
   return (
-    <div
-      id="form-container"
-      className="border-30px border-green-600 p-3"
-    >
-      <form method="POST" action="#" className="flex flex-col items-center space-y-4">
+    <div id="form-container" className="border-30px border-green-600 p-3">
+      <form
+        method="POST"
+        action="#"
+        className="flex flex-col items-center space-y-4"
+      >
         <input
           type="text"
+          onChange={handleFirstNameChange}
           name="first-name"
+          value={props.firstName}
           placeholder="First Name"
           className="p-3 bg-slate-100 m-1 w-full"
         />
         <input
           type="text"
+          onChange={handleLastNameChange}
           name="last-name"
+          value={props.lastName}
           placeholder="Last Name"
           className="p-3 bg-slate-100 m-1 w-full"
         />
         <input
           type="email"
+          onChange={handleEmailChange}
           name="email"
+          value={props.email}
           placeholder="Email"
           className="p-3 bg-slate-100 m-1 w-full"
         />
