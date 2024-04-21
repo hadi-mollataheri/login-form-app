6228d16cea503e77b097d25ce12b1f513c1e56b3
Hadi <mhadimollataheri@gmail.com>
2024-04-21 11:09:54 +0330

    Fix vetical aligment of Form.js elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,12 +20,12 @@ function Form(props) {
   return (
     <div
       id="form-container"
-      className="border-30px p-3 border-green-600 flex flex-col items-center"
+      className="border-30px p-3 border-green-600 flex flex-col items-center justify-center"
     >
       {props.isSubmit && (
         <div
           id="submit-answer"
-          className="p-3 m-1 w-full bg-blue-500 text-white text-center"
+          className="p-3 my-1 mx-auto w-full bg-blue-500 text-white text-center"
         >
           <p className="p-0 m-0`">Success! Thank you for registering</p>
         </div>
@@ -35,56 +35,56 @@ function Form(props) {
         onSubmit={handleSubmit}
         className="flex flex-col items-center w-full space-y-4"
       >
-        <div id='input-1'>
+        <div id="input-1" className="w-full">
           <input
             type="text"
             onChange={handleFirstNameChange}
             name="first-name"
             value={props.firstName}
             placeholder="First Name"
-            className="p-3 bg-slate-100 m-1 w-full my-0"
+            className="p-3 bg-slate-100 mx-auto w-full my-0"
             required
             disabled={props.isSubmit}
           />
-          <span className="self-start text-sm text-red-600 mt-0.5 mx-1">
+          <span className="text-sm text-red-600 mt-0.5">
             Please enter a first name
           </span>
         </div>
-        <div id='input-2'>
+        <div id="input-2" className="w-full">
           <input
             type="text"
             onChange={handleLastNameChange}
             name="last-name"
             value={props.lastName}
             placeholder="Last Name"
-            className="p-3 bg-slate-100 m-1 w-full"
+            className="p-3 bg-slate-100 mx-auto w-full my-0"
             required
             disabled={props.isSubmit}
           />
-          <span className="self-start text-sm text-red-600 mt-0.5 mx-1">
+          <span className="text-sm text-red-600 mt-0.5">
             Please enter a last name
           </span>
         </div>
-        <div id='input-3'>
+        <div id="input-3" className="w-full">
           <input
             type="email"
             onChange={handleEmailChange}
             name="email"
             value={props.email}
             placeholder="Email"
-            className="p-3 bg-slate-100 m-1 w-full"
+            className="p-3 bg-slate-100 mx-auto w-full my-0"
             required
             disabled={props.isSubmit}
           />
-          <span className="self-start text-sm text-red-600 mt-0.5 mx-1">
+          <span className="text-sm text-red-600 mt-0.5">
             Please enter an email
           </span>
         </div>
-        <div id='input-4' className='w-full'>
+        <div id="input-4" className="w-full">
           <input
             type="submit"
             value="Register"
-            className="p-3 mx-1 bg-green-700 text-white w-full"
+            className="p-3 bg-green-700 text-white w-full"
             disabled={props.isSubmit}
           />
         </div>
